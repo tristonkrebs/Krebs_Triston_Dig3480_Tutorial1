e6327d178fc4bd6c59cf38fbc9c8c1f06920141b
Triston Krebs <tkrebs@knights.ucf.edu>
2019-01-23 21:50:18 -0500

    Initial Commit

---
+++ Assets/scripts/playercontroller.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playercontroller : MonoBehaviour{
+
+    public float speed;
+
+    private Rigidbody rb;
+    private int count;
+    public Text countText;
+    public Text winText;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement*speed);
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        } 
+    }
+    void SetCountText ()
+    {
+        countText.text = "Count:" + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win";
+        }
+    }
+
+}
